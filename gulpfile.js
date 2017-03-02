f50ed2eade0914baa39daeb83fb6dd4b0ea37c57
Rakshan Shetty <shetty.raxx555@gmail.com>
2017-03-02 09:57:44 +0530

    :bug: Fix

--- gulpfile.js
+++ gulpfile.js
@@ -78,7 +78,7 @@ gulp.task('image', () =>
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('assets/css/**', ['sass']);
+    gulp.watch('assets/css/**/*.scss', ['sass']);
     gulp.watch('assets/js/**', ['js','jekyll-rebuild']);
     gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html'], ['jekyll-rebuild']);
 });
